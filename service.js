68a459f9c228694a2eda85eea1ae77b5e95f9a92
sumon10c <sumonchakrobarty9466@gmail.com>
2025-08-29 00:59:38 +0600

    add my call history funtion

--- service.js
+++ service.js
@@ -118,3 +118,162 @@ callBtn6.addEventListener("click",function(){
         alert("Calling Anti-Corruption Helpline .\n106")
     }
 })
+
+// Call history list
+
+const callHistoryBtn1 = document.getElementById("call-btn-1")
+const callHistoryBtn2 = document.getElementById("call-btn-2")
+const callHistoryBtn3 = document.getElementById("call-btn-3")
+const callHistoryBtn4 = document.getElementById("call-btn-4")
+const callHistoryBtn5 = document.getElementById("call-btn-5")
+const callHistoryBtn6 = document.getElementById("call-btn-6")
+
+const time = new Date().toLocaleTimeString();
+
+const clearBtn = document.getElementById("clear-history")
+const historyList = document.getElementById("call-history-list")
+
+const serviceName1 = document.getElementById("service-name-1").textContent
+const serviceNumber1 = document.getElementById("service-num-1").textContent
+
+
+const serviceName2 = document.getElementById("service-name-2").textContent
+const serviceNumber2 = document.getElementById("service-num-2").textContent
+
+
+const serviceName3 = document.getElementById("service-name-3").textContent
+const serviceNumber3 = document.getElementById("service-num-3").textContent
+
+
+const serviceName4 = document.getElementById("service-name-4").textContent
+const serviceNumber4 = document.getElementById("service-num-4").textContent
+
+
+const serviceName5 = document.getElementById("service-name-5").textContent
+const serviceNumber5 = document.getElementById("service-num-5").textContent
+
+
+const serviceName6 = document.getElementById("service-name-6").textContent
+const serviceNumber6 = document.getElementById("service-num-6").textContent
+
+
+callHistoryBtn1.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-2 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName1}</h1>
+      <p>${serviceNumber1}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+callHistoryBtn2.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-3 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName2}</h1>
+      <p>${serviceNumber2}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+
+callHistoryBtn3.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-3 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName3}</h1>
+      <p>${serviceNumber3}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+
+callHistoryBtn4.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-3 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName4}</h1>
+      <p>${serviceNumber4}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+
+callHistoryBtn5.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-3 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName5}</h1>
+      <p>${serviceNumber5}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+
+callHistoryBtn6.addEventListener("click",function(){
+    const div = document.createElement("div");
+    div.className = "bg-[#fafafa] p-3 mt-[20px] rounded-lg";
+
+    div.innerHTML = `<div class="flex items-center">
+    <div><h1 class="font-[500]">${serviceName6}</h1>
+      <p>${serviceNumber6}</p></div>
+      <div class="text-gray-500 text-[15px]">${time}</div>
+      </div>
+    `;
+    historyList.appendChild(div)
+
+})
+
+
+// history clear funtion
+
+clearBtn.addEventListener("click",function(){
+    historyList.innerHTML = "";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
